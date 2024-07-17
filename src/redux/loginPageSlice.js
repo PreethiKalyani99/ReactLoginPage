36fdb6f2c5e428ccf9f878b58019e2bbb5933156
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-07-17 13:03:09 +0530

    removes unwanted functions and variables

--- src/redux/loginPageSlice.js
+++ src/redux/loginPageSlice.js
@@ -4,22 +4,14 @@ import { signUp } from "./loginPageThunk";
 export const loginPageSlice = createSlice({
     name: 'loginPage',
     initialState: {
-        isAccountCreated: false,
         isLoading: false,
-        data: null
-    },
-    reducers: {
-        toggle:  (state) => {
-            state.isAccountCreated = !state.isAccountCreated
-        }
     },
     extraReducers: (builder) => {
         builder.addCase(signUp.pending, (state) => {
             state.isLoading = true
         })
-        .addCase(signUp.fulfilled, (state, action) => {
+        .addCase(signUp.fulfilled, (state) => {
             state.isLoading = false
-            state.data = action.payload
         })
     }
 })
